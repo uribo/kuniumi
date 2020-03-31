5b9d5e7bc9910bada790fcf5b31a53d3b6dab146
Shinya Uryu <uryu.shinya@nies.go.jp>
2020-03-31 14:50:16 +0900

    Merged ksj files

---
+++ R/ksj_bind.R
@@ -0,0 +1,29 @@
+#' Bind KSJ files in directory
+#' @param dir path
+#' @param identifier Data identifier. (e.g. "N02")
+#' @param regexp A regular expression passed on target directory.
+#' @param ... Additional arguments to parser function
+#' @export
+ksj_bind <- function(dir, identifier, regexp = NULL, translate = NULL) {
+  files <-
+    list.files(dir,
+               pattern = regexp,
+               recursive = TRUE,
+               full.names = TRUE)
+  if (is.null(translate)) {
+    d <-
+      files %>%
+      purrr::map(
+        ~ rlang::exec(paste0("read_ksj_", identifier),
+                      path = .x))
+  } else {
+    d <-
+      files %>%
+      purrr::map(
+        ~ rlang::exec(paste0("read_ksj_", identifier),
+                      path = .x,
+                      translate = translate))
+  }
+  d %>%
+    purrr::reduce(rbind)
+}
