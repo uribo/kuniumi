aacba62349ac5c8c7963eab4e75496b5c4bef9a3
Shinya Uryu <r.sh1nya.uryu@gmail.com>
2022-08-14 09:14:10 +0900

    [add]new parser
    P34 市区町村役場データ

---
+++ R/read_ksj_p34.R
@@ -0,0 +1,36 @@
+#' Kokudosuuchi P34 parser
+#' @inheritParams read_ksj_n03
+#' @description If there is no local file, specify the year and pref_code to download.
+#' @export
+read_ksj_p34 <- function(path = NULL, .pref_code = NULL, .download = FALSE) {
+  if (is.null(path)) {
+    dl_zip <-
+      zip_p34_url(.pref_code)
+    path <-
+      download_ksj_zip(dl_zip, .download = .download, source = "ksj")
+  }
+  d <-
+    sf::st_read(
+    path,
+    options = c("ENCODING=CP932"),
+    stringsAsFactors = FALSE,
+    as_tibble = TRUE)
+  d %>%
+    purrr::set_names(dplyr::recode(names(d),
+                                   P34_001 = "行政区域コード",
+                                   P34_002 = "施設分類",
+                                   P34_003 = "名称",
+                                   P34_004 = "所在地")) %>%
+    dplyr::mutate(dplyr::across(where(is.character),
+                                .fns = stringi::stri_trans_general,
+                                id = "nfkc"))
+}
+
+zip_p34_url <- function(pref_code = NULL) {
+  pref_code <-
+    sprintf("%02d", as.numeric(pref_code)) %>%
+    jpndistrict:::prefcode_validate()
+  glue::glue("https://nlftp.mlit.go.jp/ksj/gml/data/P{file_code}/P{file_code}-{year_last2}/P{file_code}-{year_last2}_{pref_code}_GML.zip", # nolint
+             file_code = "34",
+             year_last2 = "14")
+}
