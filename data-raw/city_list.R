cc50995b00e81fe811b53d6f7e977357d2ed6bfc
Shinya Uryu <uryu.shinya@tokushima-u.ac.jp>
2023-01-16 18:50:23 +0900

    [modify]コードを整理

--- data-raw/city_list.R
+++ data-raw/city_list.R
@@ -74,7 +74,6 @@ df_jp_all1724 <-
   mutate(administrative_area_code = if_else(str_detect(county_name, "市$") & !is.na(county_name),
                                             NA_character_,
                                             administrative_area_code)) %>%
-  filter(str_detect(prefecture_name, "東京都", negate = TRUE) && str_detect(admin_type, "ward", negate = TRUE)) %>%
   distinct(prefecture_name, city_name, administrative_area_code, admin_type) %>%
   assertr::verify(nrow(.) == 1747L) %>%
   left_join(df_ward %>%
@@ -98,34 +97,44 @@ df_jp_all <-
   df_jp_all %>%
   arrange(administrative_area_code)
 
-collect_city_website_url <- function(city) {
-  city %>%
-    purrr::map_chr(
-      function(.x) {
-        res <-
-          WikidataR::find_item(.x, language = "ja") %>%
-          purrr::keep(~ !is.null(.x$description) &&
-                        grepl("((village|town|city|one of).+ Japan|special ward of Tokyo)",
-                              .x$description,
-                              ignore.case = FALSE)) %>%
-          purrr::flatten() %>%
-          purrr::pluck("id")
-        if (is.null(res)) {
-          NA_character_
-        } else {
-          res %>%
-            WikidataR::get_item() %>%
-            purrr::map(c("claims", "P856", "mainsnak", "datavalue", "value")) %>%
-            purrr::flatten_chr() %>%
-            purrr::pluck(1)
+collect_city_website_url <- memoise::memoise(
+  function(city) {
+    city %>%
+      purrr::map_chr(
+        function(.x) {
+          res <-
+            WikidataR::find_item(.x, language = "ja")
+          res_id <-
+            res %>%
+            purrr::keep(~ !is.null(.x$description) &&
+                          grepl("((village|town|city|one of).+ Japan|special ward of Tokyo)",
+                                .x$description,
+                                ignore.case = FALSE)) %>%
+            purrr::flatten() %>%
+            purrr::pluck("id")
+          if (is.null(res_id)) {
+            NA_character_
+          } else {
+            res_url <-
+              res_id %>%
+              WikidataR::get_item() %>%
+              purrr::map(c("claims", "P856", "mainsnak", "datavalue", "value")) %>%
+              purrr::flatten_chr() %>%
+              purrr::pluck(1)
+            if (is.null(res_url)) {
+              res_url <- NA_character_
+            }
+            res_url
+          }
         }
-      }
-    )
-}
+      )
+  }
+)
 # collect_city_website_url("つくば市")
 # collect_city_website_url("清川村")
 # collect_city_website_url("丹波篠山市") # 新しいものもOK
 # collect_city_website_url("小美玉市")
+# collect_city_website_url("色丹村")
 # collect_city_website_url("函館市") # URLが複数、一つを選択
 
 # 同じ名前の市町村 (26) は除外しておく
@@ -141,8 +150,12 @@ if (file.exists("data-raw/city_list1724_url.csv") == FALSE) {
   df_jp_all1724_comp <-
     df_jp_all1724 %>%
     filter(!city_name %in% duplicate_city_names) %>%
-    assertr::verify(nrow(.) == 1688L) %>%
-    mutate(url = collect_city_website_url(city = city_name))
+    assertr::verify(nrow(.) == 1688L)
+
+  urls <-
+    df_jp_all1724_comp$city_name %>%
+    purrr::map_chr(collect_city_website_url)
+  df_jp_all1724_comp$url <- urls
   df_jp_all1724_comp %>%
     readr::write_csv("data-raw/city_list1724_url.csv")
 } else {
